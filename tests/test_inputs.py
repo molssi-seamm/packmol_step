e27fadc208ae0f8abbc22e90d8a3983ec935019a
Paul Saxe <psaxe@h80adf301.dhcp.vt.edu>
2022-05-25 16:42:12 -0400

    Added more tests.

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -5,6 +5,7 @@
 
 import json
 from pathlib import Path
+from seamm_util import units_class
 
 import pytest
 
@@ -27,25 +28,55 @@ def test_spherical_region(input_file):
     parameters.from_dict(tmp)
     P = parameters.current_values_to_dict()
 
-    # The golden output file.
-    output_filename = input_file.with_suffix(".out").name
-    output = test_dir / "outputs" / output_filename
+    # The golden output file and PACKMOL input file
+    output = test_dir / "outputs" / input_file.with_suffix(".out").name
+    input = test_dir / "outputs" / input_file.with_suffix(".inp").name
 
     # Create the two System Databases that are needed
     system_db = SystemDB(filename="file:seamm_db?mode=memory&cache=shared")
     tmp_db = SystemDB(filename="file:tmp_db?mode=memory&cache=shared")
 
+    # Print what we are doing. Have to fix formatting for printing...
+    PP = dict(P)
+    for key in PP:
+        if isinstance(PP[key], units_class):
+            PP[key] = "{:~P}".format(PP[key])
+
+    instance = packmol_step.Packmol()
+    instance._id = (1,)
+    instance.parameters = parameters
+
+    # Get the description
+    description = instance.description_text(PP)
+    output_text = "\n".join(description.splitlines()[1:])
+
     # And get the input for PACKMOL
     molecules, files, text, cell = packmol_step.Packmol.get_input(P, system_db, tmp_db)
 
+    output_text += "\n"
+    output_text += text
+
+    output_reference = ""
+    input_reference = ""
     if output.exists():
-        reference = output.read_text()
-        if files["input.inp"] != reference:
+        output_reference = output.read_text()
+        if output_text != output_reference:
             new_output = output.with_suffix(".out_new")
-            new_output.write_text(files["input.inp"])
+            new_output.write_text(output_text)
             print(f"Updated output written to {new_output}")
-        assert files["input.inp"] == reference
     else:
-        output.write_text(files["input.inp"])
+        output.write_text(output_text)
         print(f"New output written to {output}")
-        assert False
+
+    if input.exists():
+        input_reference = input.read_text()
+        if files["input.inp"] != input_reference:
+            new_input = input.with_suffix(".inp_new")
+            new_input.write_text(files["input.inp"])
+            print(f"Updated PACKMOL input written to {new_input}")
+    else:
+        input.write_text(files["input.inp"])
+        print(f"New PACKMOL input written to {input}")
+
+    assert output_text == output_reference
+    assert files["input.inp"] == input_reference
